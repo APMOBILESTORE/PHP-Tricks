e2afeccf749058da03f43ff8965eeb5be463fcd1
AP Mobile Store <64196459+APMOBILESTORE@users.noreply.github.com>
2023-05-26 19:34:52 +0530

    Update get_json_data_php.php

--- get_json_data_php.php
+++ get_json_data_php.php
@@ -2,3 +2,7 @@
 $people_json = file_get_contents('data.json');
 $decoded_json = json_decode($people_json, true);
 ?>
+
+<?php echo $decoded_json['name']; ?>
+<?php echo $decoded_json['address']; ?>
+<?php echo $decoded_json['mobile']; ?>
