cc91c19e457c257aa095a4f992a282418accf76b
AP Mobile Store <64196459+APMOBILESTORE@users.noreply.github.com>
2023-05-28 15:10:23 +0530

    Update get_full_user_info.php

--- get_full_user_info.php
+++ get_full_user_info.php
@@ -2,5 +2,18 @@
 $people_json = file_get_contents('https://ipinfo.io/json');
 $decoded_json = json_decode($people_json, true);
 ?>
+<?php echo $decoded_json['ip']; ?>
+<br>
 <?php echo $decoded_json['city']; ?>
+<br>
+<?php echo $decoded_json['region']; ?>
+<br>
 <?php echo $decoded_json['country']; ?>
+<br>
+<?php echo $decoded_json['loc']; ?>
+<br>
+<?php echo $decoded_json['org']; ?>
+<br>
+<?php echo $decoded_json['postal']; ?>
+<br>
+<?php echo $decoded_json['timezone']; ?>
